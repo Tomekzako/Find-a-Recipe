d522a501a2c032a2db591847d5df3149cb242c02
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-05-14 12:01:35 +0200

    Search Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,6 @@
-import axios from 'axios';
+import Search from './models/Search';
 
+const search = new Search('pizza');
+console.log(search);
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '9c3f86dfc83f56098463f1cf38883b40';
-    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-}
-getResults('pizza');
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,19 @@
-export default 'I am an exported string';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = '9c3f86dfc83f56098463f1cf38883b40';
+        try {
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
