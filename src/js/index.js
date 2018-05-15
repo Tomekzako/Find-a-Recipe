aef1092776a5bac5b925854c0749db39767ca6c7
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-05-15 10:56:35 +0200

    Search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,22 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
+const state = {};
 
-search.getResults();
+const constrolSearch = async() => {
+    // 1) Get query from view
+    const query = 'pizza';
+
+    if (query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        console.log(state.search.result);
+    }
+
+}
