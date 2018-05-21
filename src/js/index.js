903d9a7804e18a1d6c37b8daa222b57c08336bd5
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-05-21 10:31:49 +0200

    Search Results Pagination

--- src/js/index.js
+++ src/js/index.js
@@ -41,7 +41,7 @@ elements.searchResPages.addEventListener('click', e => {
     const btn = e.target.closest('.btn-inline');
     if (btn) {
         const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
         searchView.renderResults(state.search.result, goToPage);
-        console.log(goToPage);
     }
 })
