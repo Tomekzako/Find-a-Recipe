a65e5557f318a4fea7ffc0ffedeb9cad648453cd
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-06-11 12:08:12 +0200

    deleteItem method

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -1,15 +1,21 @@
-import uniqueID from 'uniqid';
+import uniqid from 'uniqid';
 
 export default class List {
     constructor() {
         this.items = [];
     }
 
-    addItem (count, unit, ingredient) {
+    addItem(count, unit, ingredient) {
         const item = {
+            id: uniqid(),
             count,
             unit,
             ingredient
         }
+    }
+
+    deleteItem(id) {
+        const index = this.items.findIndex(el => el.id === id);
+        this.items.splice(index, 1);
     }
 }
