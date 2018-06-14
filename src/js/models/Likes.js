0940e0aa4c6253b35b0a5130ca44e9782687cdfc
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-06-14 09:09:55 +0200

    isLiked, getNumLikes methods

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -13,4 +13,12 @@ export default class Likes {
         const index = this.likes.findIndex(el => el.id === id);
         this.likes.splice(index, 1);
     }
+
+    isLiked(id) {
+         return this.likes.findIndex(el => el.id === id) !== -1;
+    }
+
+    getNumLikes() {
+        return this.likes.length;
+    }
 }
