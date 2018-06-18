a4c485ac2d2f148002efd45134063c6805afe6b9
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-06-18 12:47:03 +0200

    renderLike markup

--- src/js/views/likesView.js
+++ src/js/views/likesView.js
@@ -1,4 +1,6 @@
-import { elements } from './base';
+import {
+    elements
+} from './base';
 
 export const toggleLikeBtn = isLiked => {
 
@@ -7,7 +9,23 @@ export const toggleLikeBtn = isLiked => {
 
 };
 
-
 export const toggleLikeMenu = numLikes => {
     elements.likesMenu.style.visibility = numLikes > 0 ? 'visible' : 'hidden';
+};
+
+export const renderLike = like => {
+    const markup = `
+        <li>
+            <a class="likes__link" href="#${like.id}">
+                <figure class="likes__fig">
+                    <img src="${like.img}" alt="${like.title}">
+                </figure>
+                <div class="likes__data">
+                    <h4 class="likes__name">${like.title}</h4>
+                    <p class="likes__author">${like.author}</p>
+                </div>
+            </a>
+        </li>
+    `;
+    elements.likesList.insertAdjacentHTML('beforeend', markup);
 };
