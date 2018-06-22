634994b7aca73fd65e2ed532e32423e7ab71ab64
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-06-22 14:40:36 +0200

    format count error

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -4,12 +4,14 @@ import { Fraction } from 'fractional';
 
 const formatCount = count => {
     if (count) {
-        const [int, dec] = count.toString().split('.').map(el => parseInt(el));
 
-        if(!dec) return count;
+        const newCount = Math.round(count * 10000) / 10000; 
+        const [int, dec] = newCount.toString().split('.').map(el => parseInt(el));
+
+        if(!dec) return newCount;
 
         if (int === 0) {
-            const fr = new Fraction(count);
+            const fr = new Fraction(newCount);
             return `${fr.numerator}/${fr.denominator}`;
         } else {
             const fr = new Fraction(count- int);
